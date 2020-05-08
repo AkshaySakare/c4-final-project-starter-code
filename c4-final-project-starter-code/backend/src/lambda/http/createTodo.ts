e736f2aeef26e1ec3a243da9f7fc896d4d87695d
akshay <sakshay2494@gmail.com>
2020-05-09 01:05:27 +0530

    first commit

---
+++ c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,49 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { CreateTodo } from '../../businessLogic/todos'
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
+import { createLogger } from '../../utils/logger';
+import { getUserId } from '../utils'
+
+const logger = createLogger ('Create Todo Item')
+
+export const handler = middy(
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+        try{
+            logger.info('Processing event: ', event)
+
+            const newTodo: CreateTodoRequest = JSON.parse(event.body)
+            const userId = getUserId(event)
+
+            // TODO: Implement creating a new TODO item
+            const newItem = await CreateTodo(newTodo, userId)
+            return {
+                statusCode: 201,
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({
+                    newItem
+                })
+            }
+        } catch (e) {
+            logger.error('Error: ' + e.message)
+  
+            return {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },  
+            body: e.message
+            }
+        }
+    }
+)
+
+handler.use(
+    cors({
+        credentials: true
+      })
+)
---
+++ c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,47 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+import { GenerateUploadUrl } from '../../businessLogic/todos'
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
+import { createLogger } from '../../utils/logger';
+
+const logger = createLogger ('Generate Upload URLs')
+
+export const handler =  middy(
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+        try{
+            logger.info('Processing event: ', event)
+            const todoId = event.pathParameters.todoId
+            // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+            const signedUploadUrl = await GenerateUploadUrl(todoId)
+
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({
+                    uploadUrl: signedUploadUrl
+                })
+            }
+        } catch (e){
+            logger.error('Error: ' + e.message)
+  
+            return {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },  
+            body: e.message
+            }
+        }
+    }
+)
+
+handler.use(
+    cors({
+      credentials: true
+    })
+  )
